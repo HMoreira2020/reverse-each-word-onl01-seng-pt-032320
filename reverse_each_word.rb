8fb58f3e4e88a9084140279d34e3028fa166d1f3
Heather Moreira <moreiraheather@gmail.com>
2020-03-31 02:32:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,6 @@ def reverse_each_word(phrase)
   return reversed_string 
 end 
   
+#must store elements invoked and reversed by #.collect in order to operate on them later. return value of collect is an array of invoked elements but it still doesn't modify original array. #.each return value is original array, not invoked elements 
     
 
